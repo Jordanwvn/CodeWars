010d58418297257a02dee787b6ea75d4834023e8
Jordan Van Ness <jordanwvn@gmail.com>
2018-04-20 13:36:58 -0700

    adding reversed string.

--- reversed-string.js
+++ reversed-string.js
@@ -5,9 +5,5 @@ solution('world'); // returns 'dlrow'
 *******************************************************************************/
 
 function solution(str){
-  var newString = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
-    }
-    return newString;
+  return str.split('').reverse().join('');
 }
